04134246abb286841aecfde6fb6ec301c23d33d5
ywow-ai <masyaqin37@gmail.com>
2025-03-20 13:58:43 +0700

    merge

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./page/App";
-import "./assets/main.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <div>xxx</div>
   </StrictMode>
 );
